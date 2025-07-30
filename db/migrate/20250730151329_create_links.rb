8cc53f78b76c40030321f3ee03c4502566fca351
Fabian Veliz <velizfabianhoracio@gmail.com>
2025-07-30 12:32:20 -0300

    Create Link model

---
+++ db/migrate/20250730151329_create_links.rb
@@ -0,0 +1,14 @@
+class CreateLinks < ActiveRecord::Migration[8.0]
+  def change
+    create_table :links do |t|
+      t.string :url
+      t.string :title
+      t.string :description
+      t.string :image
+      t.integer :views_count
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
