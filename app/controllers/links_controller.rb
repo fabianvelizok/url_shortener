b7d785200791c656a22b6dea8631c30d8094e4cd
Fabian Veliz <velizfabianhoracio@gmail.com>
2025-07-30 15:22:25 -0300

    Validate url field and display form errors

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,22 @@
 class LinksController < ApplicationController
   def index
     @links = current_user.links
+    @link = Link.new
+  end
+
+  def create
+    @link = current_user.links.new(link_params)
+
+    if @link.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:url)
   end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,5 @@
 class Link < ApplicationRecord
   belongs_to :user
+
+  validates :url, presence: true, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]) }
 end
