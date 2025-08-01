b00687fd8b97e71af5be7295ebc921cbbb4bd3c9
Fabian Veliz <velizfabianhoracio@gmail.com>
2025-08-01 13:46:16 -0300

    Create view model, define associations and display line chart

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -1,5 +1,6 @@
 class ViewsController < ApplicationController
   def show
+    @link.views.create!(ip: request.ip, user_agent: request.user_agent)
     redirect_to @link.url, allow_other_host: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_30_151329) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_01_161931) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,7 +19,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_30_151329) do
     t.string "title"
     t.string "description"
     t.string "image"
-    t.integer "views_count"
+    t.integer "views_count", default: 0
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -38,5 +38,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_30_151329) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "views", force: :cascade do |t|
+    t.bigint "link_id", null: false
+    t.string "user_agent"
+    t.string "ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["link_id"], name: "index_views_on_link_id"
+  end
+
   add_foreign_key "links", "users"
+  add_foreign_key "views", "links"
 end
