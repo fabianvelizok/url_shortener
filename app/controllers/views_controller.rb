fb7e629f7a7b05d92c24df5994e21fd3ff81c7e3
Fabian Veliz <velizfabianhoracio@gmail.com>
2025-08-12 14:29:11 -0300

    Make views route public

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -1,8 +1,17 @@
 class ViewsController < ApplicationController
+  skip_before_action :authenticate_user!
   before_action :set_link, only: [ :show ]
 
   def show
     @link.views.create!(ip: request.ip, user_agent: request.user_agent)
     redirect_to @link.url, allow_other_host: true
+  end
+
+  private
+
+  def set_link
+    @link = Link.find_by_id_param!(params[:id])
+  rescue ArgumentError
+    raise ActiveRecord::RecordNotFound
   end
 end
